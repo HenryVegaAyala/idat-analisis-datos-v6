b249cbc88f4b3649d53411dc0ec801aeafd11d34
Henry Vega <hp.vega@hotmail.com>
2026-07-02 20:34:46 -0500

    first commit

---
+++ sesion-1/main.py
@@ -0,0 +1,3 @@
+# Esto es un comentario no se ejecuta en la consola
+
+print("Hola mundo")  # Esto es un mensaje de saludo
---
+++ sesion-2/ejemplos/condicionales.py
@@ -0,0 +1,19 @@
+# if: "si pasa esto..."
+# elif: "Si no paso lo anterior, prueba esto"
+# else: "Si nada cumple"
+
+# If else
+edad = 15
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+# If elif else
+promedio = 11
+if promedio >= 18:
+    print("Excelente")
+elif promedio >= 13:
+    print("Aprobado") # segunda condicional
+else:
+    print("Desaprobado") # Caso final si ninguna cumple
