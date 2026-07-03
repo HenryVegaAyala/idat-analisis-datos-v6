272a531a9f5d48c6e32480c492bec868bfb4b557
Henry Vega <hp.vega@hotmail.com>
2026-07-02 20:44:06 -0500

    add basic arithmetic operations in Python

---
+++ sesion-2/ejemplos/operadores_matematicos.py
@@ -0,0 +1,17 @@
+# Operadores matemáticos en Python
+
+# aritméticos tradicionales
+suma = 5 + 5
+restar = 200 - 50
+multiplicacion = 20 * 20
+division = 20 / 2
+
+# aritméticos directos
+numero = 10
+numero += 5 # ===> esto es igual a (numero = numero + 5)
+
+numero = 5
+numero -= 5 # ===> esto es igual a (numero = numero - 5)
+
+numero = 10
+numero += -5 # ===> esto igual a (numero = numero + -5)
