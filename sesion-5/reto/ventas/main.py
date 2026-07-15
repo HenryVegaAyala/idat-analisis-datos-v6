e0ebdcfcb9ba331186de47c3e6c862fa9a5ae3a7
Henry Vega <hp.vega@hotmail.com>
2026-07-14 21:09:26 -0500

    add functions to calculate final price and greet user

--- sesion-5/reto/ventas/main.py
+++ sesion-5/reto/ventas/main.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+# Configuración de ventana
+pd.set_option("display.max_columns", None) # Va a mostrar todas las columnas
+pd.set_option("display.max_rows", None) # Va a mostrar todas las filas
+pd.set_option("display.width", None) # Se va autoajustar cada columna
+
 data = pd.read_excel("ventas_enero_2026_excel.xlsx")
 
 filtro = data["precio_unitario"] > 1000 # Filtro de resultados
